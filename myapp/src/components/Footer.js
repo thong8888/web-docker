fe16e8c3bbd5dc254e41fe0a528f98faa08708f3
thong8888 <97076698+thong8888@users.noreply.github.com>
2025-08-28 08:34:43 +0700

    update login page v1.0

--- myapp/src/components/Footer.js
+++ myapp/src/components/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../styles/footer.css";
+import iconJT from "../img/j&t.c4156e1b.svg";
+import appStore from "../img/app_store.898e5572.png";
+import googlePlay from "../img/google_play.8b8e8458.png";
+import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
   return (
     <footer className="jt-footer">
       <div className="jt-footer-main">
         <div className="jt-footer-logo">
-          <img
-            src="https://jt-express.vn/themes/jtexpress/assets/images/logo.svg"
-            alt="J&T Express"
-          />
+          <img src={iconJT} alt="J&T Express" className="login-logo" />
           <div className="jt-footer-description">
             <a>CÔNG TY TNHH MỘT THÀNH VIÊN CHUYỂN PHÁT NHANH THUẬN PHONG</a>
             <p>
@@ -17,6 +18,20 @@ const Footer = () => {
               lần 06 ngày 25/09/2018 Cơ Quan Cấp: Phòng Đăng ký kinh doanh - Sở
               kế hoạch và đầu tư TP Hồ Chí Minh
             </p>
+          </div>
+          <div className="jt-footer-certifications">
+            <img
+              src="https://jtexpress.vn/themes/jtexpress/assets/images/dmca.png"
+              alt="DMCA.com Protection Status"
+              title="DMCA.com Protection Status"
+              className="footer-certification-icon"
+            />
+            <img
+              src="https://jtexpress.vn/themes/jtexpress/assets/images/bo-cong-thuong.png"
+              alt="Bộ Công Thương"
+              title="Bộ Công Thương"
+              className="footer-certification-icon"
+            />
           </div>
         </div>
         <div className="jt-footer-quick">
@@ -52,56 +67,35 @@ const Footer = () => {
         <div className="jt-footer-app">
           <h4>Tải ứng dụng</h4>
           <div className="jt-footer-app-links">
+            <img
+              src="https://quickchart.io/chart?chs=500x500&cht=qr&chl=https://jtexpress.vn/vi/download&choe=UTF-8"
+              alt="J&T Express - Giao hàng Chuyển phát nhanh"
+              title="Link to Download"
+              className="QRcode"
+            />
+
             <a href="https://play.google.com/store/apps/details?id=com.jtexpress.vn">
               <img
-                src="https://jt-express.vn/themes/jtexpress/assets/images/icon-googleplay.svg"
+                src={googlePlay}
                 alt="Google Play"
+                className="aboutus-app-icon"
               />
             </a>
             <a href="https://apps.apple.com/vn/app/j-t-express-vietnam/id1457279222">
               <img
-                src="https://jt-express.vn/themes/jtexpress/assets/images/icon-appstore.svg"
+                src={appStore}
                 alt="App Store"
+                className="aboutus-app-icon"
               />
             </a>
           </div>
         </div>
-        <div className="jt-footer-social">
-          <h4>Kết nối với chúng tôi</h4>
-          <a
-            href="https://www.facebook.com/jtexpressvn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://jt-express.vn/themes/jtexpress/assets/images/icon-facebook.svg"
-              alt="Facebook"
-            />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCwQvQnQnQnQnQnQnQnQnQnQ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://jt-express.vn/themes/jtexpress/assets/images/icon-youtube.svg"
-              alt="YouTube"
-            />
-          </a>
-          <a
-            href="https://www.tiktok.com/@jtexpressvn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://jt-express.vn/themes/jtexpress/assets/images/icon-tiktok.svg"
-              alt="TikTok"
-            />
-          </a>
-        </div>
       </div>
       <div className="jt-footer-bottom">
-        <p>&copy; 2025 J&T Express. All rights reserved.</p>
+        <div className="jt-footer-social">
+          <SocialLinks />
+        </div>
+        <p>&copy; Copyright © 2025 J&T Express. All rights reserved.</p>
       </div>
     </footer>
   );
